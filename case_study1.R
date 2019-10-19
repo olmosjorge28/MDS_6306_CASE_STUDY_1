e1947d2a9bc2a7afacba4f18998873a3b11a9724
Jorge Olmos <jorgeolmos@Jorges-MacBook-Pro.local>
2019-10-19 09:37:48 -0500

    initial case study commit

---
+++ case_study1.R
@@ -0,0 +1,73 @@
+library(tm) #text mining library provides the stopwords() function
+library(tidyr)
+library(plyr)
+library(jsonlite)
+library(dplyr)
+library(tidyverse)
+library(RCurl)
+library(e1071)
+library(caret)
+library(ggplot2)
+
+
+brewery_data = read.csv(file.choose(), header = TRUE);
+
+beers_data = read.csv(file.choose(), header = TRUE);
+
+
+#How many breweries are present in each state?
+state_count = count(brewery_data$State)
+
+
+colnames(brewery_data)[1] = "Brewery_id"
+
+#Merge beer data with the breweries data. Print the first 6 observations and the last six observations to check the merged file.  (RMD only, this does not need to be included in the presentation or the deck.)
+beers_brewery_merge = merge(beers_data,brewery_data,"Brewery_id")
+colnames(beers_brewery_merge)[2] = "beer_name"
+colnames(beers_brewery_merge)[8] = "brewery_name"
+
+
+#Address the missing values in each column
+filtered_df = beers_brewery_merge %>% filter(!(is.na(beers_brewery_merge$Brewery_id) &&
+                                   is.na(beers_brewery_merge$beer_name) &&
+                                   is.na(beers_brewery_merge$Beer_ID) &&
+                                   is.na(beers_brewery_merge$ABV) &&
+                                   is.na(beers_brewery_merge$IBU) &&
+                                   is.na(beers_brewery_merge$Style) &&
+                                   is.na(beers_brewery_merge$Ounces) &&
+                                   is.na(beers_brewery_merge$brewery_name) &&
+                                   is.na(beers_brewery_merge$City) && 
+                                   is.na(beers_brewery_merge$State))) 
+
+
+#Compute the median alcohol content and international bitterness unit for each state. Plot a bar chart to compare.
+no_na <- na.omit(beers_brewery_merge)
+no_na$ABV
+result_abv <- aggregate(x = no_na[c("ABV","IBU")], by = no_na[c("State")], FUN = median, na.action = na.rm)
+result_ibu <- aggregate(x = no_na[c("ABV")], by = no_na[c("State")], FUN = median, na.action = na.rm)
+
+p<-ggplot(data=result, aes(x=State, y=ABV)) +
+  geom_bar(stat="identity")
+
+p<-ggplot(data=result, aes(x=State, y=IBV)) +
+  geom_bar(stat="identity")
+
+
+
+#Which state has the maximum alcoholic (ABV) beer? Which state has the most bitter (IBU) beer?
+no_na[which.max(no_na$ABV),] #KY
+no_na[which.max(no_na$IBU),] #OR
+
+#Comment on the summary statistics and distribution of the ABV variable.
+summary(no_na$ABV)
+
+
+#Is there an apparent relationship between the bitterness of the beer and its alcoholic content? Draw a scatter plot.  Make your best judgment of a relationship and EXPLAIN your answer.
+no_na %>% ggplot(aes(x = ABV, y = IBU)) + geom_point()
+
+
+
+  
+
+
+
